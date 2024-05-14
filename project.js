67e4d0b5e1e0b9628bdd514a84602bbe8a24a7df
SaadiOkba <151369710+SaadiOkba@users.noreply.github.com>
2024-05-14 21:22:35 +0200

    Add files via upload

---
+++ project.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    document.getElementsByClassName("table").style = " display : unset";
+    document.getElementById("table2").style = "display: none";
+    
+  }
+  function myfonction(){
+    document.getElementById("table2").style ="display : unset";
+        document.getElementsByClassName("table").style = " display : none"; 
+  }
+  
